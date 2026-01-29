1ac0d729c87700b0eb1edfb05940c4de360032f8
danny-andersen <danny_andersen@yahoo.com>
2026-01-29 10:14:52 +0000

    Added PI contoller interface

--- i2c-test/i2c-test.ino
+++ i2c-test/i2c-test.ino
@@ -13,7 +13,8 @@ bool leftGround() {
 
 
 bool compassReady = false;
-int8_t piOnBus = 1;
+int8_t piOnBus = 1;    //0 = on bus, any other value is an error
+int8_t nanoOnBus = 1;  //0 = on bus, any other value is an error
 
 uint16_t currentHeading = 0;
 uint16_t lastHeading = 0;
@@ -46,21 +47,7 @@ void setup() {
   Serial.println(compassReady);
 }
 
-void loop() { 
-  piOnBus = getPiStatusCmd();
-  if (piOnBus) {
-    //Failed to get a response
-    Serial.print("Failed to talk to PI: ");
-    Serial.println(piOnBus);
-  } else {
-    Serial.print("PI Ready: ");
-    Serial.print(piStatus.ready);
-    Serial.print(" Lidar: ");
-    Serial.print(piStatus.lidarStatus);
-    Serial.print(" Direction: ");
-    Serial.println(piStatus.directionToDrive);
-  }
-
+void loop() {
   systemStatus.currentBearing = getCompassBearing();
   readAttitude(&systemStatus.pitch, systemStatus.roll);
   systemStatus.batteryVoltage = getBatteryVoltageInt();
@@ -83,17 +70,28 @@ void loop() {
   } else {
     Serial.println();
   }
-  systemStatus.rightWheelSpeed = 24;
-  systemStatus.leftWheelSpeed = 20;
-  systemStatus.averageSpeed = 22;
-  systemStatus.distanceTravelled = 32;
 
-  systemStatus.proximityState = getProximityState();
-  if (systemStatus.proximityState == 0xFF) {
-    Serial.println("Timed out waiting for nano");
+
+  nanoOnBus = getStatusCmd();
+  if (nanoOnBus) {
+    Serial.print("Failed to send to Nano: ");
+    Serial.println(nanoOnBus);
+  } else {
+    Serial.print("Nano Proximity: ");
+    Serial.print(periStatus.proximityState);
+    Serial.print(" avg speed: ");
+    Serial.print(periStatus.averageSpeed);
+    Serial.print(" distance: ");
+    Serial.println(periStatus.distanceTravelled);
+  }
+
+  // nanoOnBus = getProximityState();
+  if (nanoOnBus) {
+    Serial.print("Failed to send to Nano: ");
+    Serial.println(nanoOnBus); 
   } else if (systemStatus.proximityState > 0) {
     if (checkFrontProximity(systemStatus.proximityState)) {
-      Serial.print("FRONT ");
+      Serial.print("Nano FRONT ");
       if (checkFrontRightProximity(systemStatus.proximityState)) Serial.print("RIGHT ");
       if (checkFrontLeftProximity(systemStatus.proximityState)) Serial.print("LEFT ");
     }
@@ -106,41 +104,89 @@ void loop() {
   }
 
   // Send status to PI
-  int8_t piStatus = sendSystemStatus();
-  if (piStatus) {
+  int8_t piOnBus = sendSystemStatus();
+  if (piOnBus) {
     Serial.print("Failed to send system status to PI: ");
-    Serial.println(piStatus);
+    Serial.println(piOnBus);
   }
 
-  sweep(distances);
-  // Array to hold arcs that represent similar distances, i.e. an object or obstacle in front
-  for (int i = 0; i < MAX_NUMBER_OF_OBJECTS_IN_SWEEP; i++) {
-    arcs[i].avgDistance = 0;
-  }
-  for (int i = 0; i < MAX_NUMBER_OF_OBJECTS_IN_SWEEP; i++) {
-    arcs[i].avgDistance = 0;
-  }
-  uint8_t numObjects = findObjectsInSweep(distances, NUMBER_OF_ANGLES_IN_SWEEP, arcs, MAX_NUMBER_OF_OBJECTS_IN_SWEEP);
-  // uint8_t numObjects = 5;
-  for (int i = 0; i < numObjects; i++) {
-    Serial.print("Rel Dirn: ");
-    Serial.print(arcs[i].centreDirection);
-    Serial.print(" Width: ");
-    Serial.print(arcs[i].width);
-    Serial.print(" Distance: ");
-    Serial.println(arcs[i].avgDistance);
-  }
-  //Send Obstacles to PI
-  int8_t obsStatus = sendObstacles(systemStatus.currentBearing, numObjects, &arcs[0]);
-  if (!obsStatus) {
-    Serial.print("Sent all ");
-    Serial.print(numObjects);
-    Serial.println(" obstacles");
-
+  piOnBus = getPiStatusCmd();
+  if (piOnBus) {
+    //Failed to get a response
+    Serial.print("Failed to talk to PI: ");
+    Serial.println(piOnBus);
   } else {
-    Serial.print("Failed to send all obstacles: ");
-    Serial.println(obsStatus);
+    Serial.print("PI Ready: ");
+    Serial.print(piStatus.ready);
+    Serial.print(" Lidar: ");
+    Serial.print(piStatus.lidarStatus);
+    Serial.print(" Direction: ");
+    Serial.println(piStatus.directionToDrive);
+    if (piStatus.lidarStatus > 0) {
+      if (checkFrontProximity(piStatus.lidarStatus)) {
+        Serial.print("LIDAR FRONT ");
+        if (checkFrontRightProximity(piStatus.lidarStatus)) Serial.print("RIGHT ");
+        if (checkFrontLeftProximity(piStatus.lidarStatus)) Serial.print("LEFT ");
+      }
+      if (checkRearProximity(piStatus.lidarStatus)) {
+        Serial.print("LIDAR REAR ");
+        if (checkRearRightProximity(piStatus.lidarStatus)) Serial.print("RIGHT ");
+        if (checkRearLeftProximity(piStatus.lidarStatus)) Serial.print("LEFT ");
+      }
+      Serial.println();
+    }
   }
 
-  delay(30000);
+  if (!getPiStatusCmd()) {
+    //Pi is available
+    //OR the lidar proximity status in with the IR proximity sensors
+    systemStatus.proximityState |= piStatus.lidarStatus;
+  }
+
+  Serial.print("Combined Proximity: ");
+  if (checkFrontProximity(systemStatus.proximityState)) {
+    Serial.print("FRONT ");
+    if (checkFrontRightProximity(systemStatus.proximityState)) Serial.print("RIGHT ");
+    if (checkFrontLeftProximity(systemStatus.proximityState)) Serial.print("LEFT ");
+  }
+  if (checkRearProximity(systemStatus.proximityState)) {
+    Serial.print("REAR ");
+    if (checkRearRightProximity(systemStatus.proximityState)) Serial.print("RIGHT ");
+    if (checkRearLeftProximity(systemStatus.proximityState)) Serial.print("LEFT ");
+  }
+  systemStatus.proximityState |= piStatus.lidarStatus;
+  Serial.println();
+
+
+  // sweep(distances);
+  // // Array to hold arcs that represent similar distances, i.e. an object or obstacle in front
+  // for (int i = 0; i < MAX_NUMBER_OF_OBJECTS_IN_SWEEP; i++) {
+  //   arcs[i].avgDistance = 0;
+  // }
+  // for (int i = 0; i < MAX_NUMBER_OF_OBJECTS_IN_SWEEP; i++) {
+  //   arcs[i].avgDistance = 0;
+  // }
+  // uint8_t numObjects = findObjectsInSweep(distances, NUMBER_OF_ANGLES_IN_SWEEP, arcs, MAX_NUMBER_OF_OBJECTS_IN_SWEEP);
+  // // uint8_t numObjects = 5;
+  // for (int i = 0; i < numObjects; i++) {
+  //   Serial.print("Rel Dirn: ");
+  //   Serial.print(arcs[i].centreDirection);
+  //   Serial.print(" Width: ");
+  //   Serial.print(arcs[i].width);
+  //   Serial.print(" Distance: ");
+  //   Serial.println(arcs[i].avgDistance);
+  // }
+  // //Send Obstacles to PI
+  // int8_t obsStatus = sendObstacles(systemStatus.currentBearing, numObjects, &arcs[0]);
+  // if (!obsStatus) {
+  //   Serial.print("Sent all ");
+  //   Serial.print(numObjects);
+  //   Serial.println(" obstacles");
+
+  // } else {
+  //   Serial.print("Failed to send all obstacles: ");
+  //   Serial.println(obsStatus);
+  // }
+
+  delay(1000);
 }
