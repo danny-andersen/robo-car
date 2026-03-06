78d7cd9ebe6ac07ee625fe14bbe8c9c83b217a82
danny-andersen <danny_andersen@yahoo.com>
2026-03-06 16:58:02 +0000

    Allowed the map viewer to show how the map wasa built up over multple moves + sweeps

--- lidar-viewer/map_viewer.py
+++ lidar-viewer/map_viewer.py
@@ -1,37 +1,149 @@
+import glob
+import re
+
 import numpy as np
+import math
 from PyQt5 import QtWidgets, QtGui, QtCore
+
 from world_map_widget import WorldMapWidget   # your existing widget
+
 
 class OfflineViewer(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.setFocus()
         # Load data
-        self.map = np.load("map.npy")
+        self.index = 0
+        self.map_history = self.load_map_history()
+        self.map = self.map_history[self.index]
+        self.map_shape = self.map.shape
         print("Number of set pixels:", np.sum(self.map > 0))
         print(np.unique(self.map, return_counts=True))  # Check unique values and their counts
         self.poses = np.loadtxt("poses.csv", delimiter=",")
 
+        # self.scans = np.load("scans.npy", allow_pickle=True)
+        # print("Number of scans:", len(self.scans))
+        # print("Scan shape:", self.scans[0].shape)
+
+        self.frontiers = np.load("frontiers.npy", allow_pickle=True)
+        print("Number of frontiers saved:", len(self.frontiers))
+        
+        
         # Create map widget
         self.map_widget = WorldMapWidget(
             map_pixels=self.map.shape[0],
+            maps=self.map_history,
             resolution_m=0.02
         )
+        
+        
+        self.diff_mode = False
         self.setCentralWidget(self.map_widget)
+        self.show_scans = True
 
-        # Timer to animate robot path
-        self.index = 0
+        # Timer to animate robot path and map updates
         self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.update_view)
-        self.timer.start(50)
+        self.timer.timeout.connect(self.nextPose)
+        self.timer.start(2000)  # Update every 2 seconds
+        
+        self.activateWindow()
+        self.raise_()
+        self.setFocus()
+
+
+    def load_map_history(self):
+        files = glob.glob(f"map_*.npy")
+
+        # Sort numerically by index
+        files.sort(key=lambda f: int(re.findall(r"map_(\d+)\.npy", f)[0]))
+        print(f"Found {len(files)} map files.")
+        map_history = [np.load(f) for f in files]
+        return map_history
+
+
+    def nextPose(self):
+        self.index = min(len(self.poses) - 1, self.index + 1)
+        self.update_view()
+        
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Space:
+            if self.timer.isActive():
+                self.timer.stop()
+            else:
+                self.timer.start(2000)  # 0.5 FPS
+        elif event.key() in (QtCore.Qt.Key_Return, QtCore.Qt.Key_Enter):
+            self.index = 0
+            print("Reset to start")
+        elif event.key() == QtCore.Qt.Key_Left:
+                self.index = max(0, self.index - 1)
+                self.timer.stop()
+                self.update_view()
+        elif event.key() == QtCore.Qt.Key_Right:
+            self.index = min(len(self.map_history) - 1, self.index + 1)
+            self.timer.stop()
+            self.update_view()
+        elif event.key() == QtCore.Qt.Key_D:
+            self.diff_mode = not self.diff_mode
+            self.update_view()
+
+
+    def mousePressEvent(self, event):
+        self.setFocus()
+        super().mousePressEvent(event)
 
     def update_view(self):
         if self.index >= len(self.poses):
             return
+        
+        if len(self.poses.shape) == 1:    
+            _, x, y, th = self.poses
+        else:
+            _, x, y, th = self.poses[self.index]
+            
+        # scan_mm = self.scans[self.index]
+        # print(f"{self.index}: {np.argmin(scan_mm)} -> {np.argmax(scan_mm)}")
+        # Convert scan to world coords only if needed
+        # scan_world = self.scan_to_world(scan_mm, x, y, th) if self.show_scans else None 
+        scan_world = None 
 
-        _, x, y, th = self.poses[self.index]
-        self.map_widget.update_map_and_pose(self.map, (x, y, th))
-        self.index += 1
+        if (len(self.frontiers) > self.index):
+            clusters, targets = self.frontiers[self.index]
+        else:
+            clusters, targets = [], []
+
+        self.map_widget.update_map_pose_scan_frontiers(
+            self.index,
+            self.diff_mode,
+            (x, y, th),
+            scan_world,
+            clusters,
+            targets
+        )
+        self.raise_()
+        self.setFocus()
+
+
+    def scan_to_world(self, scan_mm, x_mm, y_mm, theta):
+        angles = -np.deg2rad(np.arange(360))
+        d = np.array(scan_mm, dtype=float)
+        mask = (d > 50) & (d < 12000)
+
+        if not np.any(mask):
+            return np.zeros((0, 2))
+
+        xr = d[mask] * np.cos(angles[mask])
+        yr = d[mask] * np.sin(angles[mask])
+
+        c = math.cos(theta)
+        s = math.sin(theta)
+
+        wx = x_mm + (c * xr - s * yr)
+        wy = y_mm + (s * xr + c * yr)
+
+        return np.vstack((wx, wy)).T
+
 
 
 app = QtWidgets.QApplication([])
--- lidar-viewer/world_map_widget.py
+++ lidar-viewer/world_map_widget.py
@@ -1,6 +1,8 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
 import numpy as np
 import math
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QImage, QColor, QLinearGradient, QPainter
 
 
 class WorldMapWidget(QtWidgets.QWidget):
@@ -10,8 +12,10 @@ class WorldMapWidget(QtWidgets.QWidget):
     - Robot pose moves within the map.
     """
 
-    def __init__(self, map_pixels, resolution_m, parent=None):
+    def __init__(self, map_pixels, maps, resolution_m, parent=None):
         super().__init__(parent)
+        
+        self.setFocusPolicy(QtCore.Qt.NoFocus)
 
         self.map_pixels = map_pixels        # number of grid cells per side
         self.resolution = resolution_m      # meters per cell
@@ -19,6 +23,21 @@ class WorldMapWidget(QtWidgets.QWidget):
         self._map = np.zeros((map_pixels, map_pixels), dtype=np.uint8)
         self._pose = (0.0, 0.0, 0.0)        # x_mm, y_mm, theta_rad
         self._map_image = None
+        self.map_history = maps
+
+        self.lut = self.build_qt_colormap()
+
+        # self.slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        # self.slider.setMinimum(0)
+        # self.slider.setMaximum(len(maps) - 1)
+        # self.slider.setValue(0)
+        # self.slider.valueChanged.connect(self.on_slider_change)
+        # self.slider.setFocusPolicy(QtCore.Qt.NoFocus)
+
+        self._scan_world = None   # Nx2 array of (wx_mm, wy_mm)
+
+        self._clusters = None
+        self._targets = None
 
         self.setMinimumSize(400, 400)
         self.setSizePolicy(QtWidgets.QSizePolicy.Expanding,
@@ -33,18 +52,124 @@ class WorldMapWidget(QtWidgets.QWidget):
 
         self._map = occ_grid.astype(np.uint8)
         self._pose = pose_mm
+        
         self._update_qimage()
         self.update()
+
+    def update_map_pose_scan_frontiers(self, map_index, diff_mode, pose_mm,  scan_world, clusters, targets):
+ 
+        self._pose = pose_mm
+        # self.slider.setValue(map_index)
+        self._scan_world = scan_world
+        self._clusters = clusters
+        self._targets = targets
+        self._map = self.map_history[map_index]
+        if (diff_mode and map_index > 0):
+            self._map_image = self.compute_map_diff(map_index - 1, map_index, self.lut)
+        else:
+            self._update_qimage(self._map)
+        self.update()
+   
+    def on_slider_change(self, index):
+        self.current_index = index
+        self.update()
+
+
+    def _draw_target_cluster(self, painter, target_rect):
+        if not self._clusters:
+            return
+
+        h = self.map_pixels
+        w = self.map_pixels
+
+        painter.setPen(QtGui.QPen(QtGui.QColor(0, 255, 0), 3))
+
+        for cluster in self._clusters:
+            for (gx, gy) in cluster:
+                sx = target_rect.left() + (gx / w) * target_rect.width()
+                sy = target_rect.top() + (gy / h) * target_rect.height()
+                painter.drawPoint(QtCore.QPointF(sx, sy))
+
+    def _draw_scan(self, painter, target_rect):
+        if self._scan_world is None or len(self._scan_world) == 0:
+            return
+
+        h = self.map_pixels
+        w = self.map_pixels
+
+        for wx_mm, wy_mm in self._scan_world:
+            # world mm → meters
+            x_m = wx_mm / 1000.0
+            y_m = wy_mm / 1000.0
+
+            # meters → grid index
+            gx = x_m / self.resolution
+            gy = y_m / self.resolution
+
+            # flip Y for display
+            gy = h - 1 - gy
+
+            if not (0 <= gx < w and 0 <= gy < h):
+                continue
+
+            # grid → screen
+            sx = target_rect.left() + (gx / w) * target_rect.width()
+            sy = target_rect.top() + (gy / h) * target_rect.height()
+
+            painter.setPen(QtGui.QPen(QtGui.QColor(255, 0, 0), 2))
+            painter.drawPoint(QtCore.QPointF(sx, sy))
 
     # ---------------------------------------------------------
     # Numpy → QImage
     # ---------------------------------------------------------
-    def _update_qimage(self):
-        h, w = self._map.shape
-        data = self._map.copy().tobytes()
+    def _update_qimage(self, map):
+        h, w = map.shape
+        data = map.copy().tobytes()
+        # print(f"Updating QImage with shape {map.shape} and data length {len(data)}")
         self._map_image = QtGui.QImage(
             data, w, h, w, QtGui.QImage.Format_Grayscale8
         )
+
+    def build_qt_colormap(self):
+        # Create a 256×1 image to hold the gradient
+        img = QImage(256, 1, QImage.Format_RGB32)
+
+        # Define a Jet-like gradient
+        grad = QLinearGradient(0, 0, 255, 0)
+        grad.setColorAt(0.0, QColor(0, 0, 128))     # dark blue
+        grad.setColorAt(0.25, QColor(0, 0, 255))    # blue
+        grad.setColorAt(0.5, QColor(0, 255, 0))     # green
+        grad.setColorAt(0.75, QColor(255, 255, 0))  # yellow
+        grad.setColorAt(1.0, QColor(255, 0, 0))     # red
+
+        painter = QPainter(img)
+        painter.fillRect(img.rect(), grad)
+        painter.end()
+
+        # Extract LUT as list of QColor
+        lut = [QColor(img.pixel(i, 0)) for i in range(256)]
+        return lut
+
+    def compute_map_diff(self, map1_index, map2_index, lut):
+        """
+        map1, map2: uint8 numpy arrays (grayscale)
+        lut: list of 256 QColor entries
+        returns: QImage with heatmap diff
+        """
+
+        # Absolute difference (NumPy only)
+        diff = np.abs(self.map_history[map1_index].astype(np.int16) - self.map_history[map2_index].astype(np.int16)).astype(np.uint8)
+
+        h, w = diff.shape
+        img = QImage(w, h, QImage.Format_RGB32)
+
+        # Apply LUT pixel-by-pixel
+        for y in range(h):
+            row = diff[y]
+            for x in range(w):
+                img.setPixelColor(x, y, lut[row[x]])
+
+        return img
 
     # ---------------------------------------------------------
     # Painting
@@ -79,6 +204,13 @@ class WorldMapWidget(QtWidgets.QWidget):
 
         # Draw robot pose relative to this fixed map
         self._draw_robot(painter, target_rect)
+
+        # Draw scan points
+        # self._draw_scan(painter, target_rect)
+        
+        self._draw_target_cluster(painter, target_rect)
+        # self._draw_frontier_targets(painter, target_rect)
+
 
     # ---------------------------------------------------------
     # Draw robot pose in global map coordinates
