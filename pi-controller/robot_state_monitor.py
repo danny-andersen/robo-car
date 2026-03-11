fcdd2d226c4cfc303db3ad57578c2799c719b55e
danny-andersen <danny_andersen@yahoo.com>
2026-03-11 17:23:03 +0000

    Fixed sending heading, distance, confidence, etc. to  SLAM

--- pi-controller/robot_state_monitor.py
+++ pi-controller/robot_state_monitor.py
@@ -15,29 +15,37 @@ class RobotStateMonitor:
         self.explorer_manager : ExplorationManager = explorer_manager
         self.statesSinceLastSweep = []
         self.startDriveHeading = None
+        self.move_confidence = -1.0
+        self.distance_travelled_since_last_sweep = 0.0
+        self.current_bearing = 0.0
+        self.driveStarted = False
 
     def update_state(self, robotState, current_bearing, distance_travelled):
 
-        avg_moving_bearing = current_bearing
+        self.avg_moving_bearing = current_bearing
+        self.current_bearing = current_bearing
         if robotState != self.last_state:
             if (self.last_state == config.ROBOT_STATE_NAMES.index("SWEEP")):
                 self.statesSinceLastSweep = []
+                self.move_confidence = -1.0
+                self.distance_travelled_since_last_sweep = 0.0
+                self.current_bearing = 0.0
+                self.driveStarted = False
             self.statesSinceLastSweep.append(robotState)
             if (robotState == config.ROBOT_STATE_NAMES.index("SWEEP")):
                 # We have just started a sweep, so we can use the distance travelled since the last sweep to update SLAM with the movement since the last sweep, which will help to keep the map and frontier information accurate for decision making during the sweep, but we need to determine how much confidence to place in the distance travelled measurement based on the sequence of states since the last sweep
-                distance_travelled = distance_travelled * 10.0  # convert cm to mm for SLAM and explorer manager updates
-                move_confidence = 1.0
+                self.distance_travelled_since_last_sweep = distance_travelled * 10.0  # convert cm to mm for SLAM and explorer manager updates
+                self.move_confidence = 1.0
                 # We are starting a sweep - determine whether distance travelled since last sweep can be used to update SLAM, 
                 # or whether we should discard it as unreliable (e.g. due to wheel slip)
                 # Basically if the sequence of states is something like ROTATE, DRIVE then SWEEP, then we can use the distance travelled to update SLAM, but if it is something like ROTATE -> SWEEP or multiple ROTATEs -> SWEEP, 
                 # then we have good confidence that the distance measured is accurate
-                driveStarted = False
                 move_while_rotating = False
                 rotated_during_drive = False
                 for state in self.statesSinceLastSweep:
-                    if not driveStarted and state == config.ROBOT_STATE_NAMES.index("DRIVE"):
-                        driveStarted = True
-                        move_confidence = 0.9  # Default confidence in distance travelled measurement
+                    if not self.driveStarted and state == config.ROBOT_STATE_NAMES.index("DRIVE"):
+                        self.driveStarted = True
+                        self.move_confidence = 0.9  # Default confidence in distance travelled measurement
                     elif (state == config.ROBOT_STATE_NAMES.index("ROTATING") or \
                         state == config.ROBOT_STATE_NAMES.index("ROTATING_LEFT") or \
                             state == config.ROBOT_STATE_NAMES.index("ROTATING_RIGHT") or \
@@ -45,49 +53,54 @@ class RobotStateMonitor:
                                     state == config.ROBOT_STATE_NAMES.index("ROTATING_RIGHT_BLOCKED_LEFT") or \
                                         state == config.ROBOT_STATE_NAMES.index("ROTATING_FRONT_BLOCKED_BACKING_OUT") or \
                                             state == config.ROBOT_STATE_NAMES.index("ROTATING_REAR_BLOCKED_GO_FORWARD")) \
-                          and driveStarted:
+                          and self.driveStarted:
                         # We have started driving and then rotated, so distance travelled is likely to be inaccurate due to wheel slip during rotation, so discard it
                         print("Low confidence in distance travelled due to rotation in drive:", [config.ROBOT_STATE_NAMES[s] for s in self.statesSinceLastSweep])
-                        move_confidence = 0.2
+                        self.move_confidence = 0.2
                         rotated_during_drive = True
                         break
                     if state == config.ROBOT_STATE_NAMES.index("ROTATING_FRONT_BLOCKED_BACKING_OUT") or \
                                             state == config.ROBOT_STATE_NAMES.index("ROTATING_REAR_BLOCKED_GO_FORWARD"):
                         #We didnt rotate on the spot, so cant trust the location
                         move_while_rotating = True
-                if not driveStarted and not move_while_rotating:
+                        self.startDriveHeading = None # Clear the start drive heading as we can no longer rely on it for SLAM updates during the sweep if we rotated during the drive
+                if not self.driveStarted and not move_while_rotating:
                     # We never started driving, we just probably rotated on the spot, so we can be confident that we didnt actually move, but we can still use the current bearing to update SLAM during the sweep
                     print("Did not start driving - no distance travelled")
-                    distance_travelled = 0
-                    move_confidence = 0.9 #High degree of confidence that we did not actually move, but we can still use the current bearing to update SLAM during the sweep
-                if self.startDriveHeading is not None and move_confidence == 1.0:
+                    self.distance_travelled_since_last_sweep = 0
+                    self.move_confidence = 0.9 #High degree of confidence that we did not actually move, but we can still use the current bearing to update SLAM during the sweep
+                    self.startDriveHeading = None # Clear the start drive heading as we can no longer rely on it for SLAM updates during the sweep if we rotated during the drive
+                if self.startDriveHeading is not None:
                     bearing_diff = abs(current_bearing - self.startDriveHeading)
                     if bearing_diff > 30 and bearing_diff < 330:
                         print("Poor confidence in distance travelled since last sweep due to large change in bearing during drive. Start bearing:", self.startDriveHeading, "Current bearing:", current_bearing)
-                        move_confidence = 0.6
+                        self.move_confidence = 0.6
                     # Calculate an average bearing during the drive to use for SLAM updates, which may be more accurate than just using the current bearing at the end of the drive if there was some rotation during the drive
-                    avg_moving_bearing = config.average_heading(self.startDriveHeading, current_bearing)
-                    move_confidence = 0.9 # Reduce confidence slightly as we are now relying on the average bearing calculation which may be inaccurate if there was significant rotation during the drive, but it is likely to still be more accurate than just using the current bearing at the end of the drive in this case
+                    self.avg_moving_bearing = config.average_heading(self.startDriveHeading, current_bearing)
+                    self.move_confidence = 0.9 # Reduce confidence slightly as we are now relying on the average bearing calculation which may be inaccurate if there was significant rotation during the drive, but it is likely to still be more accurate than just using the current bearing at the end of the drive in this case
                     self.startDriveHeading = None
-                    print("Average moving bearing during drive:", avg_moving_bearing)
+                    print("Average moving bearing during drive:", self.avg_moving_bearing)
                 if rotated_during_drive:
-                    if driveStarted:
+                    if self.driveStarted:
                         print("Moved while rotating - low confidence in distance travelled due to rotation in drive:", [config.ROBOT_STATE_NAMES[s] for s in self.statesSinceLastSweep])
-                        move_confidence = 0.1
+                        self.move_confidence = 0.1
                     else:
                         #Zero distance travelled
-                        distance_travelled = 0
-                        move_confidence = 0.3
+                        self.distance_travelled_since_last_sweep = 0
+                        self.startDriveHeading = None # Clear the start drive heading as we can no longer rely on it for SLAM updates during the sweep if we rotated during the drive
+                        self.move_confidence = 0.3
                         print("Moved while rotating without starting drive - low confidence in distance travelled due to rotation without drive:", [config.ROBOT_STATE_NAMES[s] for s in self.statesSinceLastSweep])
                 # If the sequence contains BACK_OUT or OFF_GROUND, then we should discard the distance travelled since last sweep, as it is likely to be very inaccurate
                 if config.ROBOT_STATE_NAMES.index("BACK_OUT") in self.statesSinceLastSweep or config.ROBOT_STATE_NAMES.index("OFF_GROUND") in self.statesSinceLastSweep:
                     print("Low confidence in distance travelled since last sweep due to state sequence:", [config.ROBOT_STATE_NAMES[s] for s in self.statesSinceLastSweep])
-                    move_confidence = 0.2
+                    self.startDriveHeading = None # Clear the start drive heading as we can no longer rely on it for SLAM updates during the sweep if we rotated during the drive
+                    self.distance_travelled_since_last_sweep = 0
+                    self.move_confidence = 0.2
                 if config.ROBOT_STATE_NAMES.index("INIT") in self.statesSinceLastSweep:
-                    move_confidence = 1.0
-                    distance_travelled = 0
+                    self.move_confidence = 0.0
+                    self.distance_travelled_since_last_sweep = 0
                 # Finally check the final bearing at the end of the drive is reasonably close to the bearing at the start of the drive (within 30 degrees), otherwise we may have had significant rotation during the drive which would make the distance travelled measurement inaccurate, so discard it
-                print("Distance travelled since last sweep:", distance_travelled, "mm with confidence", move_confidence)
+                print(f"Distance travelled since last sweep: {self.distance_travelled_since_last_sweep} mm on avg bearing {self.avg_moving_bearing} with confidence {self.move_confidence}")
             if robotState == config.ROBOT_STATE_NAMES.index("DRIVE"):
                 if self.startDriveHeading is None:
                     self.startDriveHeading = current_bearing
@@ -113,11 +126,6 @@ class RobotStateMonitor:
                 # Trigger a save of the map and targets for debugging and analysis
                 self.save_queue.put("save")
             self.last_state = robotState
-        else:
-            if (robotState == config.ROBOT_STATE_NAMES.index("SWEEP")):
-                # State hasn't changed, but if we are in a sweep state then the robot is stationary and we can keep updating SLAM with new LIDAR scans to keep the map and frontier information fresh for decision making
-                distance_travelled = 0
-                move_confidence = 1.0
 
         #Process the latest LIDAR scan for SLAM and obstacle avoidance    
         scan = self.reader.latest_scan
@@ -126,7 +134,7 @@ class RobotStateMonitor:
             if (robotState == config.ROBOT_STATE_NAMES.index("SWEEP")):
                 # We are stationary during a sweep, so update SLAM
                 # print("Updating map with new scan during sweep. Current bearing:", current_bearing, "Distance travelled since last sweep:", distance_travelled, "mm with confidence", distance_confidence)
-                self.explorer_manager.slam.update(scan, current_bearing, avg_moving_bearing, distance_travelled, move_confidence)
+                self.explorer_manager.slam.update(scan, self.current_bearing, self.avg_moving_bearing, self.distance_travelled_since_last_sweep, self.move_confidence)
             else:
                 # Moving or rotating - process scan for obstacle avoidance only
                 processProximityScan(scan, robotState)
