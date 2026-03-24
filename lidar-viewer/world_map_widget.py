4b360497566d5352c4b34223afdbc2724b7991a9
danny-andersen <danny_andersen@yahoo.com>
2026-03-24 17:30:41 +0000

    Draw ultrasonic obstacle arcs and target candidates and scores

--- lidar-viewer/world_map_widget.py
+++ lidar-viewer/world_map_widget.py
@@ -73,10 +73,14 @@ class WorldMapWidget(QtWidgets.QWidget):
         self.status_history = []
         self.obstacle_history = []
         self.candidate_target_history = []
+        self.hover_candidate = None
         self._pose = (0.0, 0.0, 0.0)        # x_mm, y_mm, theta_rad
         self._map_image = None
         self.show_grid = True
         self.show_bearing_line = True
+        self.show_ultrasonic = True
+        self.show_candidates = True
+        self.show_target = True
         self.zoom = 1.0
         self.pan_x = 0
         self.pan_y = 0
@@ -108,6 +112,7 @@ class WorldMapWidget(QtWidgets.QWidget):
         #     self.targets = np.loadtxt("slam_logs/targets.csv", delimiter=",")
         # else:
         #     print("No target file found.")
+        self.compute_global_gain_range()
         
     def load_map_history(self):
         files = glob.glob(f"slam_logs/map_*.npy")
@@ -140,8 +145,8 @@ class WorldMapWidget(QtWidgets.QWidget):
                 self.status_history.append(tuple(data["status"]))
             self.obstacle_history.append(data["obstacles"])
 
-        print(f"Frontier clusters: {self.cluster_history}")
-        print(f"Candidate targets: {self.candidate_target_history}")
+        # print(f"Frontier clusters: {self.cluster_history}")
+        # print(f"Candidate targets: {self.candidate_target_history}")
     # def load_cluster_history(self):
     #     files = glob.glob(f"slam_logs/clusters_*.npy")
 
@@ -225,8 +230,8 @@ class WorldMapWidget(QtWidgets.QWidget):
             else:
                 self.timer.start(2000)  # 0.5 FPS
         elif event.key() in (QtCore.Qt.Key_Return, QtCore.Qt.Key_Enter):
-            self.index = 0
-            self.parent.load_data()  # Reload data to reset to initial state
+            self.init_data()
+            self.load_data()  # Reload data to reset to initial state
             print("Reset to start")
             # Reset start of animation and reload data
             if self.timer.isActive():
@@ -251,6 +256,15 @@ class WorldMapWidget(QtWidgets.QWidget):
         elif event.key() == QtCore.Qt.Key_D:
             self.diff_mode = not self.diff_mode
             self.update_view()
+        elif event.key() == QtCore.Qt.Key_O:
+            self.show_ultrasonic = not self.show_ultrasonic
+            self.update()        
+        elif event.key() == QtCore.Qt.Key_C:
+            self.show_candidates = not self.show_candidates
+            self.update()        
+        elif event.key() == QtCore.Qt.Key_T:
+            self.show_target = not self.show_target
+            self.update()        
         else:
             super().keyPressEvent(event)  # Pass to parent for default handling
         
@@ -286,13 +300,34 @@ class WorldMapWidget(QtWidgets.QWidget):
         self.current_index = index
         self.update()
 
-    def gain_to_color(self, gain, min_gain, max_gain):
-        t = (gain - min_gain) / (max_gain - min_gain + 1e-6)
+    def compute_global_gain_range(self):
+        all_gains = []
+
+        for pose_candidates in self.candidate_target_history:
+            for _, gain in pose_candidates:
+                all_gains.append(gain)
+
+        if not all_gains:
+            self.global_min_gain = 0
+            self.global_max_gain = 1
+            return
+
+        self.global_min_gain = min(all_gains)
+        self.global_max_gain = max(all_gains)
+        
+        if self.global_max_gain == self.global_min_gain:
+            #No variation in gain - force it
+            self.global_min_gain = self.global_max_gain / 2
+
+    def gain_to_color(self, gain):
+        t = (gain - self.global_min_gain) / (self.global_max_gain - self.global_min_gain + 1e-6)
         r = int(255 * t)
         g = int(255 * (1 - t))
         return QColor(r, g, 0)  # red→green
 
     def draw_candidate_poses(self, painter, target_rect):
+        if not getattr(self, "show_candidates", False):
+            return
 
         if len(self.candidate_target_history) < self.index:            
             return
@@ -304,19 +339,64 @@ class WorldMapWidget(QtWidgets.QWidget):
         min_gain, max_gain = min(gains), max(gains)
 
         for (pose, gain) in candidate_poses:
-            color = self.gain_to_color(gain, min_gain, max_gain)
+            color = self.gain_to_color(gain)
             painter.setPen(QPen(color, 2))
             painter.setBrush(color)
 
             px = pose[0] / 1000.0
             py = pose[1] / 1000.0
             # world → map pixel → screen
-            # gx = px / self.resolution
-            # gy = (self.map_pixels - 1) - (py / self.resolution)
             map_pos = self.convert_point_to_map_coords((px, py), target_rect)
             if map_pos:
                 sx, sy = map_pos
                 painter.drawEllipse(QPointF(sx, sy), 5, 5)
+
+
+    def draw_hover_candidate(self, painter, target_rect):
+        if not self.mouse_pos or  \
+            not getattr(self, "show_candidates", False) or \
+            len(self.candidate_target_history) < self.index:
+            return False
+
+        hover_candidate = None
+
+        # How close the mouse must be to count as "hovering"
+        hover_radius = 8
+        candidate_poses = self.candidate_target_history[self.index]
+        for (px, py), gain in candidate_poses:
+            # Convert world → grid
+            map_pos = self.convert_point_to_map_coords((px/1000.0, py/1000.0), target_rect)
+            if map_pos:
+                sx, sy = map_pos
+            else:
+                #Not on current map
+                return False
+
+            # Distance from mouse
+            dx = self.mouse_pos.x() - sx
+            dy = self.mouse_pos.y() - sy
+            if dx*dx + dy*dy <= hover_radius * hover_radius:
+                hover_candidate = (gain, sx, sy)
+                break
+
+        if not hover_candidate:
+            return False
+
+        gain, sx, sy = hover_candidate
+
+        text = f"gain={gain}"
+
+        painter.setPen(QColor(255, 0, 0))
+
+        # Background box
+        # fm = painter.fontMetrics()
+        # w = fm.horizontalAdvance(text) + 8
+        # h = fm.height() + 4
+        # painter.drawRect(QPointF(sx + 10, sy - h - 10), w, h)
+
+        # Text
+        painter.drawText(QPointF(sx + 14, sy - 14), text)
+        return True
 
 
     def draw_cluster_centroids(self, painter, target_rect):
@@ -335,6 +415,8 @@ class WorldMapWidget(QtWidgets.QWidget):
 
 
     def draw_chosen_target(self, painter, target_rect):
+        if not getattr(self, "show_target", False):
+            return
         
         if len(self.target_history) > self.index:
             tx, ty = self.target_history[self.index]
@@ -359,6 +441,67 @@ class WorldMapWidget(QtWidgets.QWidget):
             
         # else:
         #     print("No target set")
+
+    def draw_ultrasonic_obstacles(self, painter, target_rect):
+        if not getattr(self, "show_ultrasonic", False):
+            return
+
+        x_mm, y_mm, rtheta = self._pose
+
+        rx = x_mm / 1000.0
+        ry = y_mm / 1000.0
+        
+        pen = QPen(QColor(255, 255, 0, 180))
+        pen.setWidth(2)
+        painter.setPen(pen)
+
+        for obs in self.obstacle_history[self.index]:
+            bearing = obs.get("bearing", 0)
+            width = obs.get("width", 0)
+            dist_cm = obs.get("avgDistance", 0)
+
+            # Skip invalid obstacles
+            if width <= 0 or dist_cm <= 0:
+                continue
+
+            dist_m = dist_cm / 100.0
+
+            # Convert to radians, allowing for 0 is straight up
+            bearing_rad = math.radians(90 - bearing)
+            half_width = math.radians(width) / 2.0
+
+            # Arc start/end angles in world frame
+            a1 = self.normalize_angle_rad(bearing_rad - half_width)
+            a2 = self.normalize_angle_rad(bearing_rad + half_width)
+
+            # print(f"Arc mid: {bearing}, distance: {dist_m} arc: {math.degrees(a1)} -> {math.degrees(a2)}")
+            # Generate arc angles
+            angles = self.generate_arc_angles(a1, a2, steps=20)
+
+            # Draw arc as a polyline
+            pts = []
+            for ang in angles:
+                wx = rx + dist_m * math.cos(ang)
+                wy = ry + dist_m * math.sin(ang)
+
+                # print(f"world {rx}, {ry} {math.degrees(ang)} {dist_m} -> {wx}, {wy}")
+                screen = self.convert_point_to_map_coords((wx,wy), target_rect)
+                
+                if (screen):
+                    sx, sy = screen
+                    # print(f"map {sx, sy}")
+                    pts.append(QPointF(sx, sy))
+                else:
+                    print(f"Nulling Arc outside of map: Arc mid: {bearing}, distance: {dist_m} arc: {math.degrees(a1)} -> {math.degrees(a2)}")
+                    # print(f"point world {rx}, {ry} {math.degrees(ang)} {dist_m} -> {wx}, {wy}")
+                    obs["avgDistance"] = 0
+                    pts = []
+                    break
+
+
+            # Draw the arc
+            for i in range(len(pts) - 1):
+                painter.drawLine(pts[i], pts[i+1])
 
     def _draw_scan(self, painter, target_rect):
         if self._scan_world is None or len(self._scan_world) == 0:
@@ -523,6 +666,58 @@ class WorldMapWidget(QtWidgets.QWidget):
             "E"
         )
 
+    def draw_gain_legend(self, painter, target_rect):
+        if not getattr(self, "show_candidates", False):
+            return
+
+        if len(self.candidate_target_history) == 0:            
+            return
+
+
+        # Legend size
+        legend_width = 60
+        legend_height = 160
+        margin = 10
+
+        # Position inside the map area (bottom-right corner of target_rect)
+        x0 = target_rect.right() - legend_width - margin
+        y0 = target_rect.bottom() - legend_height - margin
+        # print(f"Max: {max_gain}, min: {min_gain}, x0 {x0}, y0{y0}")
+
+        # Background box
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(QColor(0, 0, 0, 160))
+        painter.drawRect(int(x0), int(y0), legend_width, legend_height)
+
+        # Gradient bar area
+        bar_x = x0 + 20
+        bar_y = y0 + 25
+        bar_w = 20
+        bar_h = legend_height - 50
+
+        # Draw continuous vertical gradient using gain_to_color()
+        for i in range(bar_h):
+            # t goes from 0 (top) to 1 (bottom)
+            t = i / (bar_h - 1)
+
+            # Map t back to a gain value
+            gain = self.global_max_gain - t * (self.global_max_gain - self.global_min_gain)
+
+            # Use your canonical color function
+            color = self.gain_to_color(gain)
+
+            painter.setPen(color)
+            painter.drawLine(QLineF(bar_x, bar_y + i, bar_x + bar_w, bar_y + i))
+
+        # Labels
+        painter.setPen(QColor(255, 255, 255))
+        painter.drawText(QPointF(bar_x + bar_w + 5, bar_y + 5), f"{self.global_max_gain:.0f}")
+        painter.drawText(QPointF(bar_x + bar_w + 5, bar_y + bar_h), f"{self.global_min_gain:.0f}")
+
+        # Title
+        painter.drawText(QPointF(x0 + 10, y0 + 15), "Gain")
+
+
     def draw_mouse_coordinates(self, painter, target_rect):
         if self.mouse_pos is None:
             return
@@ -573,12 +768,26 @@ class WorldMapWidget(QtWidgets.QWidget):
         painter.setPen(QColor(255, 255, 0))
         painter.drawText(
             QPointF(target_rect.left() + 10, target_rect.top() + 20),
-            f"{timestamp.strftime('%d/%m/%y %H:%M:%S')}"
+            f"Index {self.index} at {timestamp.strftime('%d/%m/%y %H:%M:%S')}"
         )
         painter.drawText(
             QPointF(target_rect.left() + 10, target_rect.top() + 40),
             f"{tempC/10}°C RH: {humidity/10}% Battery: {battperc:.1f}%")
-        
+
+
+    def generate_arc_angles(self, a1, a2, steps=20):
+        if a1 <= a2:
+            # normal arc
+            return [a1 + (a2 - a1) * (i / steps) for i in range(steps + 1)]
+        else:
+            # wrap-around arc (e.g. 350° to 10°)
+            return [
+                self.normalize_angle_rad(a1 + ((a2 + 2*math.pi - a1) * (i / steps)))
+                for i in range(steps + 1)
+            ]
+
+    def normalize_angle_rad(self, a):
+        return a % (2 * math.pi)        
         
     def convert_point_to_map_coords(self, point, target_rect):
         (x_m, y_m) = point
@@ -590,7 +799,7 @@ class WorldMapWidget(QtWidgets.QWidget):
         # Flip Y because image coordinates have origin at top-left
         gy = self.map_pixels - 1 - gy
 
-        # If robot is outside the map, don't draw it
+        # If is outside the map, don't draw it
         if not (0 <= gx < self.map_pixels and 0 <= gy < self.map_pixels):
             print(f"{x_m},{y_m} -> {gx},{gy} is outside the map!")
             return
@@ -602,7 +811,7 @@ class WorldMapWidget(QtWidgets.QWidget):
         
         return (sx, sy)
         
-    def draw_bearing_line(self, painter, target_rect):
+    def draw_bearing_line(self, painter, target_rect, draw_coords):
         if self.mouse_pos is None:
             return
 
@@ -640,22 +849,23 @@ class WorldMapWidget(QtWidgets.QWidget):
         self.draw_arrowhead(painter, rsx, rsy, msx, msy)
 
         # Compute bearing and distance for label
-        
-        dx = x_m - rx_m
-        dy = y_m - ry_m
-        dist = (dx*dx + dy*dy) ** 0.5
        
-        bearing_rad = math.atan2(dx, dy)
-        bearing_deg = math.degrees(bearing_rad)
-        if bearing_deg < 0:
-            bearing_deg += 360
+        if draw_coords: 
+            dx = x_m - rx_m
+            dy = y_m - ry_m
+            dist = (dx*dx + dy*dy) ** 0.5
+        
+            bearing_rad = math.atan2(dx, dy)
+            bearing_deg = math.degrees(bearing_rad)
+            if bearing_deg < 0:
+                bearing_deg += 360
 
-        # Draw bearing label near cursor
-        painter.setPen(QColor(255, 255, 0))
-        painter.drawText(
-            QPointF(msx + 10, msy - 10),
-            f"({bearing_deg:.1f}°,{dist:.2f}m)"
-        )        
+            # Draw bearing label near cursor
+            painter.setPen(QColor(255, 255, 0))
+            painter.drawText(
+                QPointF(msx + 10, msy - 10),
+                f"({bearing_deg:.1f}°,{dist:.2f}m)"
+            )        
 
     def draw_arrowhead(self, painter, x1, y1, x2, y2):
         # Arrowhead size
@@ -722,15 +932,19 @@ class WorldMapWidget(QtWidgets.QWidget):
         
         self.draw_candidate_poses(painter, target_rect)
         self.draw_chosen_target(painter, target_rect)
-
+        self.draw_ultrasonic_obstacles(painter, target_rect)
+        
         # Draw robot pose relative to this fixed map
         self._draw_robot(painter, target_rect)
 
         self.draw_scale_bar(painter, target_rect)
         self.draw_axis_labels(painter, target_rect)        
+        self.draw_gain_legend(painter, target_rect)
 
+        candidate_coords_shown = self.draw_hover_candidate(painter, target_rect)
         if self.show_bearing_line:
-            self.draw_bearing_line(painter, target_rect)
+            self.draw_bearing_line(painter, target_rect, not candidate_coords_shown)
+
         self.draw_mouse_coordinates(painter, target_rect)
         self.draw_status(painter, target_rect)
         
